20141c8426b3b1005e3a77e4387896bac3d9d05e
kaylee <kayleekute@outlook.com>
2018-10-29 20:21:53 -0400

    second commit

--- annaworld.py
+++ annaworld.py
@@ -5,4 +5,8 @@ import webbrowser
 def main():
     #comment 1
     webbrowser.get('windows-default').open('google.com')
+
+    #math fun
+    print("5^2 = " + str(5^2))
+    print("5^2 is really: " + str(math.pow(5,2)))
 main()
